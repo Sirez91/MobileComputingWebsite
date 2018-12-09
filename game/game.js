3363e2dc2b58dd34b098f9af709dd9de439a0f06
Marc Staebler <marc.staebler@outlook.de>
2018-12-09 13:40:57 +0100

    changed the moving direction for orientation

--- game/game.js
+++ game/game.js
@@ -237,9 +237,9 @@ const orientationHandler = function (event) {
 function getOrienttion(event) {
     var horizontalOrientation = event.gamma;
     if (horizontalOrientation > 5) {
-        direction = 1;
+        direction = -1;
     } else if (horizontalOrientation < -5) {
-        direction = -1
+        direction = 1
     } else {
         direction = 0;
     }
